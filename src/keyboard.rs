de5f73b7aa0484b39f7c5242bc7fbafa901adea4
Antoine POPINEAU <antoine@popineau.eu>
2023-11-05 12:18:10 +0100

    Prevent single CTRL to insert space.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -267,10 +267,23 @@ pub async fn handle(greeter: Arc<RwLock<Greeter>>, input: KeyEvent, ipc: Ipc) ->
     },
 
     // Handle free-form entry of characters.
-    KeyEvent { code: KeyCode::Char(c), .. } => insert_key(&mut greeter, c).await,
+    KeyEvent {
+      code: KeyCode::Char(c),
+      modifiers: KeyModifiers::NONE,
+      ..
+    } => insert_key(&mut greeter, c).await,
 
     // Handle deletion of characters.
-    KeyEvent { code: KeyCode::Backspace, .. } | KeyEvent { code: KeyCode::Delete, .. } => delete_key(&mut greeter, input.code).await,
+    KeyEvent {
+      code: KeyCode::Backspace,
+      modifiers: KeyModifiers::NONE,
+      ..
+    }
+    | KeyEvent {
+      code: KeyCode::Delete,
+      modifiers: KeyModifiers::NONE,
+      ..
+    } => delete_key(&mut greeter, input.code).await,
 
     _ => {}
   }
