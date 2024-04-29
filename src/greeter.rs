c5fa133170ace0995a903b0d2c10133336ed3465
Antoine POPINEAU <antoine@popineau.eu>
2024-04-29 09:14:52 +0200

    Added infrastructure to do integration testing.

--- src/greeter.rs
+++ src/greeter.rs
@@ -149,6 +149,10 @@ pub struct Greeter {
 
   // Style object for the terminal UI
   pub theme: Theme,
+  // Display the current time
+  pub time: bool,
+  // Time format
+  pub time_format: Option<String>,
   // Greeting message (MOTD) to use to welcome the user.
   pub greeting: Option<String>,
   // Transaction message to show to the user.
@@ -193,6 +197,7 @@ impl Greeter {
       selected: 0,
     };
 
+    #[cfg(not(test))]
     greeter.parse_options().await;
 
     greeter.sessions = Menu {
@@ -464,11 +469,15 @@ impl Greeter {
       self.secret_display = SecretDisplay::Character(asterisk);
     }
 
+    self.time = self.config().opt_present("time");
+
     if let Some(format) = self.config().opt_str("time-format") {
       if StrftimeItems::new(&format).any(|item| item == Item::Error) {
         eprintln!("Invalid strftime format provided in --time-format");
         process::exit(1);
       }
+
+      self.time_format = Some(format);
     }
 
     if self.config().opt_present("user-menu") {
